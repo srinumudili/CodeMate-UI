bc431530fdd734fa720aef65e457d118604a442e
srinumudili <sekharsrinu200@gmail.com>
2025-07-17 16:02:23 +0530

    redirect to /login if not authenticated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,38 @@
+import { useDispatch, useSelector } from "react-redux";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "./utils/constants";
+import { addUser } from "./utils/userSlice";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+  const userData = useSelector((store) => store.user);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      if (userData) return;
+      try {
+        const res = await axios.get(`${BASE_URL}/profile/view`, {
+          withCredentials: true,
+        });
+
+        const user = res.data;
+        dispatch(addUser(user));
+      } catch (error) {
+        if (error?.response?.status === 401) {
+          navigate("/login");
+        } else {
+          console.error("Error fetching user", error);
+        }
+      }
+    };
+    fetchUser();
+  }, [dispatch, navigate]);
+
   return (
     <>
       <Header />
